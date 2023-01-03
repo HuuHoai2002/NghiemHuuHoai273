76a8efbf41bedc946f31bbce0388921c3ebeaa37
Huuhoai2002 <huuhoai0511@gmail.com>
2023-01-03 10:15:25 +0700

    First_commit_NHH273

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NghiemHuuHoai273.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
